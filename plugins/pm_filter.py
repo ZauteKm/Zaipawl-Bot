28592a223953cf7c1dff8c22eb0b1b5ae3286f46
Zaute Km <77600757+ZauteKm@users.noreply.github.com>
2021-11-25 16:35:55 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -781,9 +781,15 @@ async def auto_filter(client, msg, spoll=False):
             await message.delete()
         except Exception as e:
             logger.exception(e)
-            await message.reply_photo(photo="https://telegra.ph/file/5ca387cf9f686aae4ef06.png", caption=cap, reply_markup=InlineKeyboardMarkup(btn))
+         put = await message.reply_photo(photo="https://telegra.ph/file/5ca387cf9f686aae4ef06.png", caption=cap, reply_markup=InlineKeyboardMarkup(btn))
+         await asyncio.sleep(300)
+         await put.delete()
+         await message.delete()
     else:
-        await message.reply_photo(photo="https://telegra.ph/file/5ca387cf9f686aae4ef06.png", caption=cap, reply_markup=InlineKeyboardMarkup(btn))
+         pit = await message.reply_photo(photo="https://telegra.ph/file/5ca387cf9f686aae4ef06.png", caption=cap, reply_markup=InlineKeyboardMarkup(btn))
+         await asyncio.sleep(300)
+         await pit.delete()
+         await message.delete()
     if spoll:
         await msg.message.delete()
         
